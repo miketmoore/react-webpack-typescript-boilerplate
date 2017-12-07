1c9a096275850fc5428ee68f2c2dca7dc2cdde30
Michael Moore <michael.moore@contrastsecurity.com>
2017-12-07 12:12:12 -0500

    use emotion

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,7 +1,14 @@
 import * as React from "react"
+import styled, { css } from "react-emotion"
 export interface HelloProps { compiler: string; framework: string }
-export const Hello = (props: HelloProps) => (
-  <h1>
+export const Hello = (props: any) => (
+  <h1 className={props.className}>
     Hello from {props.compiler} and {props.framework}!
   </h1>
 )
+
+export const HelloStyled = styled(Hello)`
+  background-color: red;
+  color: green;
+  outline: 1px solid #000;
+`
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-import { Hello } from "./components/Hello"
+import { Hello, HelloStyled, HelloProps } from "./components/Hello"
 
+const config: HelloProps = {
+  compiler: "TypeScript",
+  framework: "React"
+}
 ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
+  <div>
+    <Hello {...config} />
+    <HelloStyled {...config} />
+  </div>,
   document.getElementById("example")
 )
